8c731e1d576f35a10413c19002ef68f6ed243b0d
Tonny Nguyen <148969983+totodesigns@users.noreply.github.com>
2024-05-28 23:24:54 +0200

    accessibility, scrolltotop update, flow update on 50/50

--- scrolltotop.js
+++ scrolltotop.js
@@ -1,18 +1,15 @@
-// Get the button:
 let scrollbutton = document.getElementById("scrollBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
     scrollbutton.style.display = "block";
   } else {
     scrollbutton.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
