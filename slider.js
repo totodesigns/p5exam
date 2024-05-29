b3a9e3a36f1fdbb381f38da61f32bd875407fb45
askovmedia <Cfaj@outlook.com>
2024-05-29 11:04:26 +0200

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,33 +1,23 @@
-// // __ Slider __
-
 let testimonials = document.getElementsByClassName('testimonial');
 let currentIndex = 0;
 
-// VIS CURRENTINDEX
 showTestimonial(currentIndex);
 
-// LYT TIL VENSTRE PIL
 let leftArrows = document.getElementsByClassName('leftarrow');
-
 for (let i = 0 ; i < leftArrows.length ; i++) {
     leftArrows[i].addEventListener('click', function() {
 
         if (currentIndex === 0) {
-            //TRUE
             currentIndex = testimonials.length - 1;
             showTestimonial(currentIndex);
-
         } else {
-            //FALSE
             currentIndex = currentIndex - 1;
             showTestimonial(currentIndex);
         }
     });
 }
 
-// LYT TIL HØJRE PIL
 let rightArrows = document.getElementsByClassName('rightarrow');
-
 for (let i = 0; i < rightArrows.length; i++) {
     rightArrows[i].addEventListener('click', function() {
 
@@ -41,9 +31,6 @@ for (let i = 0; i < rightArrows.length; i++) {
     });
 }
 
-// NÅR PIL ER KLIKKET, AKTIVER DENNE FUNKTION.
-// KØRER ALLE TESTIMONIAL INDEX'ER IGENNEM OG SER, OM DE ER LIG MED CURRENTINDEX.
-// FJERNER 'active'-CLASS HVIS NEJ, TILFØJER HVIS JA
 function showTestimonial(index) {
     for (let i = 0; i < testimonials.length; i++) {
         if (i === index) {
